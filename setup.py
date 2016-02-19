0fe7137b96e8de32b469af06e17279cd43dcf11b
nilesh1013 <nilesh.sharma7@gmail.com>
2016-02-19 17:33:31 +0400

    master: added initial setupfile

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='check_springboot',
+      version='0.1',
+      description='check springboot app Nagios plugin',
+      url='https://github.com/nilesh1013/check_springboot',
+      author='Nilesh Sharma',
+      author_email='nileshsharma7@gmail.com',
+      license='MIT',
+      packages=['check_springboot'],
+      zip_safe=False)
